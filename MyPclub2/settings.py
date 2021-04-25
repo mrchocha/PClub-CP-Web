083f314875ada86e519749960755029cb2126535
Jeet Karia <50065408+JeetKaria06@users.noreply.github.com>
2021-04-25 16:58:16 +0530

    Secret key vanishes

--- MyPclub2/settings.py
+++ MyPclub2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'it_ghc79!)2&nworw==98d2(@40@+=zd)g=tw&e-^8=3*svh81'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
